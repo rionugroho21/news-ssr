2f681bc34a129d79ee91c629b91da8f6f3371036
Rio Nugroho <rio.nugrosz@gmail.com>
2019-09-16 22:42:40 +0700

    change content

--- src/routes/Article/index.jsx
+++ src/routes/Article/index.jsx
@@ -27,6 +27,14 @@ class Article extends Component {
             <p class="articlebox-tag"><span>Fashion</span></p>
             <h2><a href="#">Makeup tutorial tips in the month of January 2018</a></h2>
           </div>
+          <div class="articlebox">
+            <p class="articlebox-tag"><span>Fashion</span></p>
+            <h2><a href="#">Makeup tutorial tips in the month of January 2018</a></h2>
+          </div>
+          <div class="articlebox">
+            <p class="articlebox-tag"><span>Fashion</span></p>
+            <h2><a href="#">Makeup tutorial tips in the month of January 2018</a></h2>
+          </div>
         </div>
 
       </div>
